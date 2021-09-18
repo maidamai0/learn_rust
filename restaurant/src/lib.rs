af57df3f2965db136e1d27c80adbbd135ade0599
maidamai0 <yuantonghao@gmail.com>
2021-09-18 23:29:55 +0800

    feat: 7 restrant

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,6 +1,6 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waiting_list() {}
+    pub mod hosting {
+        pub fn add_to_waiting_list() {}
         fn seat_at_table() {}
     }
 
@@ -9,4 +9,40 @@ mod front_of_house {
         fn serve_order() {}
         fn take_payment() {}
     }
+}
+
+fn serve_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        println!("{}", "I'm fixing the order");
+        cook_order();
+        super::serve_order();
+    }
+    fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("banana"),
+            }
+        }
+    }
+}
+
+pub fn eat_at_restaurant() {
+    println!("{}", "I'm eating at the front of the house");
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("{}", meal.toast);
+
+    crate::front_of_house::hosting::add_to_waiting_list();
+    front_of_house::hosting::add_to_waiting_list();
 }
